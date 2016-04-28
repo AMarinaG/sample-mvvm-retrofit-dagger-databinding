05a9ed847b8865ea5e9c558c201232d8a99435c7
amarina <amarina@ezentis.com>
2016-04-28 18:07:24 +0200

    observableField repository, update when received data

--- app/src/main/java/es/amarinag/mvvm_databinging_dagger2/domain/usecase/LoadRepositoriesUseCase.java
+++ app/src/main/java/es/amarinag/mvvm_databinging_dagger2/domain/usecase/LoadRepositoriesUseCase.java
@@ -14,6 +14,7 @@ import rx.Observable;
 public class LoadRepositoriesUseCase extends BaseUseCase{
   @Inject
   GithubRepository githubRepository;
+
   @Inject
   public LoadRepositoriesUseCase(ThreadExecutor executor) {
     super(executor);
--- app/src/main/java/es/amarinag/mvvm_databinging_dagger2/ui/viewmodel/RepositoryDetailViewModel.java
+++ app/src/main/java/es/amarinag/mvvm_databinging_dagger2/ui/viewmodel/RepositoryDetailViewModel.java
@@ -1,7 +1,9 @@
 package es.amarinag.mvvm_databinging_dagger2.ui.viewmodel;
 
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.databinding.ObservableBoolean;
+import android.databinding.ObservableField;
 import android.util.Log;
 import es.amarinag.mvvm_databinging_dagger2.domain.usecase.DetailRepositoryUseCase;
 import es.amarinag.mvvm_databinging_dagger2.model.Repository;
@@ -12,7 +14,7 @@ import rx.Observable;
  * Created by AMarinaG on 28/04/2016.
  */
 public class RepositoryDetailViewModel extends BaseObservable {
-  private Repository repository;
+  private ObservableField<Repository> repository = new ObservableField<>();
   private ObservableBoolean showLoading = new ObservableBoolean(true);
 
   @Inject
@@ -22,18 +24,23 @@ public class RepositoryDetailViewModel extends BaseObservable {
   public RepositoryDetailViewModel() {
   }
 
-  public Repository getRepository() {
+  public ObservableField<Repository> getRepository() {
     return repository;
   }
 
   public void setRepository(Repository repository) {
-    this.repository = repository;
+    this.repository.set(repository);
+  }
+
+  public Repository getRepositoryData() {
+    return repository.get();
   }
 
   public void refreshData(String owner, String repository) {
     Observable<Repository> repositoryObservable = detailRepositoryUseCase.invoke(owner, repository);
     repositoryObservable.subscribe(repo -> {
-          this.repository = repo;
+          Log.wtf("C[_]", "repo: " + repo);
+          setRepository(repo);
           setShowLoading(false);
         }, throwable -> Log.e("C[_]", throwable.getMessage(), throwable)
     );
